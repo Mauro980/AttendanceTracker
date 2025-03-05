4f57ae3ead352577018f9d206c8e2ec9a83292b4
Mauro dos Anjos <mauriciodosanjos980@gmail.com>
2025-03-05 23:44:27 +0800

    Added a few stiff

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import Screens.LoginScreen;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World Lets go!");
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                LoginScreen login = new LoginScreen();
+                login.show();
+            }
+        });
     }
 }
